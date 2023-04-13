8f63cf0e370ff0c3a5f2387454a3b4155ed074f2
lorenzvoss <lorenz.voss@tu-dortmund.de>
2023-04-13 16:02:01 +0200

    Player movement und rotation angefangen

--- Miniprojekt/Assets/PlayerMovement.cs
+++ Miniprojekt/Assets/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //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
     public float speed;
 
     // Start is called before the first frame update
@@ -19,8 +20,18 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Movement(){
+        
+        //Bewegungsvektor berechnen
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
 
-        transform.Translate(Vector3.right * speed * Time.deltaTime * Input.GetAxis("Horizontal"));
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * Input.GetAxis("Vertical"));
+        //Blickrichtung vom Spieler anpassen
+        if(movement != Vector3.zero){
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+
+        //Spielerposition anpassen
+        transform.Translate(movement * speed * Time.deltaTime);
     }
 }
