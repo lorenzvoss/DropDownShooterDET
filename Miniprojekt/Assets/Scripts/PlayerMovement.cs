23cad70beb39e708bda4d87c6f3ad0fa83572988
LORENZPC\loren <lorenz.voss@tu-dortmund.de>
2023-04-19 15:23:10 +0200

    Waffe verschoben und auf Leertaste Bullet in Blickrichtung schießen

--- Miniprojekt/Assets/Scripts/PlayerMovement.cs
+++ Miniprojekt/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
     //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
     public float speed;
+    public GameObject bullet;
+    public GameObject pistol;
 
     private Animator animator;
 
@@ -15,25 +17,36 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
+    {
         Movement();
+        Shoot();
     }
-    
 
-    void Movement(){
-        
+
+    void Movement()
+    {
+
         //Bewegungsvektor berechnen
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
 
-        animator.SetFloat("Speed", movement.magnitude);        
+        animator.SetFloat("Speed", movement.magnitude);
         //Blickrichtung vom Spieler anpassen
-        if(movement != Vector3.zero){
+        if (movement != Vector3.zero)
+        {
             transform.rotation = Quaternion.LookRotation(movement);
         }
 
         //Spielerposition anpassen
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bullet, pistol.transform.position, pistol.transform.rotation);
+        }
     }
 }
