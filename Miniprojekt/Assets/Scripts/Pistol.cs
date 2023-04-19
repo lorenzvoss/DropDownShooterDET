fedb012a076bd5c31e2c00210dba7163ae529182
LORENZPC\loren <lorenz.voss@tu-dortmund.de>
2023-04-19 17:10:47 +0200

    Pistole guck in Richtung Maus und schießt Patronen in diese Richtung

---
+++ Miniprojekt/Assets/Scripts/Pistol.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    public Camera mainCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            Vector3 vector = raycastHit.point;
+            vector.y = 1.4f;
+            //Debug.Log(vector);
+            transform.LookAt(vector);
+            //transform.position = vector;
+        }
+    }
+}
