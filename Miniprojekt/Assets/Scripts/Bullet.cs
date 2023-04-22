56e958ca929a3cdbe1ba8fe51d1efa11ca645a62
LORENZPC\loren <lorenz.voss@tu-dortmund.de>
2023-04-22 12:28:11 +0200

    Destroy Bullets wenn außerhalb des Spielfelds

--- Miniprojekt/Assets/Scripts/Bullet.cs
+++ Miniprojekt/Assets/Scripts/Bullet.cs
@@ -10,5 +10,15 @@ public class Bullet : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime, Space.Self);
+        if (transform.position.y < 0)
+        {
+            Destroy(gameObject);
+        }else if(transform.position.x < -12 || transform.position.x > 12)
+        {
+            Destroy(gameObject);
+        }else if (transform.position.z < -8 || transform.position.z > 16)
+        {
+
+        }
     }
 }
